05a835b33af8f2ea74a0266eea9fb68b319a8751
Nivedha-2003 <144347346+Nivedha-2003@users.noreply.github.com>
2023-09-22 20:32:12 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,12 @@
+def linear_search_product (productlist, targetproduct):
+ indices=[]
+ for i,product in enumerate(productlist):
+  if product==targetproduct:
+   indices. append (i)
+ return indices
+#example usage :
+
+products=["pen","pencil","eraser","pen","pencil","pen"]
+target=input("ENTER THE TARGET: ")
+result=linear_search_product(products,target)
+print(result)
